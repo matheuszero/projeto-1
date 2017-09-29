b232a68a587a77dd4682c175c2ef8bf7034883f8
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2017-09-29 10:58:22 -0300

    Alteração de nome dos arquivos

---
+++ create_users.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['dados'])) {
+        $_SESSION['dados'] = array();
+    } 
+?>
+<html>
+	<head>
+	</head>
+	<body>
+		<h1>Cadastro</h1>
+		<form action="bau.php" method="POST">
+        	Nome: <input type="text" name="nome"><br>
+        	Senha: <input type="password" name="senha"><br>
+        	Telefone: <input type="number" name="numero"><br>
+        	email: <input type="text" name="email"><br>
+        	<input type="submit">
+    	</form>
+	</body>
+</html>
