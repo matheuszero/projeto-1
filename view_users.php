f5ee8fa0ba6386693a8ff05e56f66d3773d968e3
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2017-10-06 11:12:13 -0300

    ok

--- view_users.php
+++ view_users.php
@@ -12,7 +12,7 @@
 	<body>
 		<?php foreach ($_SESSION['dados'] as $chave => $item): ?>
 			<ul>
-				<li><?= $item[0] , " - ", $item[2] ," - ", $item[3] , " - ", $item[4] ?><a href="delete.php?id=<?=$chave?>"> Remover </a></li>
+				<li><?= $item[0] , " - ", $item[2] ," - ", $item[3] , " - ", $item[4] ?></li>
 			</ul>
 		<?php endforeach ?>
 		<a href="index.php"> Voltar </a>
