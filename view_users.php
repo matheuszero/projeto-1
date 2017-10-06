6ed7dec48ff634229d03c262468e75f355aeaf3c
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2017-10-06 11:06:15 -0300

    reajuste

--- view_users.php
+++ view_users.php
@@ -10,11 +10,11 @@
 	<head>
 	</head>
 	<body>
-		<?php foreach ($_SESSION['dados'] as $item): ?>
+		<?php foreach ($_SESSION['dados'] as $chave => $item): ?>
 			<ul>
-				<li><?= $item[0] , " - ", $item[2] ," - ", $item[3] , " - ", $item[4] ?></li>
+				<li><?= $item[0] , " - ", $item[2] ," - ", $item[3] , " - ", $item[4] ?><a href="delete.php?id=<?=$chave?>"> Remover </a></li>
 			</ul>
 		<?php endforeach ?>
 		<a href="index.php"> Voltar </a>
 	</body>
-</html>
+</html>	
