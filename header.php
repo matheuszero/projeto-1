92655bb75e27d7cc80463321c15486d9778c18d6
rhodolfosantana <rhoodolfo.dodo@hotmail.com>
2017-10-15 14:42:23 -0300

    Implementado o head

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="">
+		<style type="text/css">
+			#menu ul {
+			padding:6px;
+			background-color:black;
+			font:100% Tahoma;
+			}
+
+			#menu ul li { display: inline; }
+ 
+			#menu ul li a {
+			background-color:black;
+			color: white;
+			text-decoration: none;
+			border-bottom:1px solid white;
+			padding: 1px 35px;
+			}
+ 
+			#menu ul li a:hover {
+			background-color:white;
+			color: black;
+			border-bottom:45px solid white;
+			}
+			
+		</style>
+	</head>
+		<header>
+			<img src="">
+					
+						<nav id="menu">
+						    <ul>
+						        <li>
+						        <li><a href="">Cadastro </a></li>
+						        <li><a href="">Entrar</a></li>
+								<li><a href="">Home </a></li>
+								<li><a href="">Fundadores</a></li>
+								<li><a href="">Usuários</a></li>
+							</ul>
+						</nav>
+	</header>
+
