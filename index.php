f2c42b4db5c02844c466c01976b8d6a5b2cce9a0
lipeh777 <filipeizidorio@outlook.com>
2017-10-19 18:18:54 -0300

    bootstrap

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/estiloLogin.css">
 		
+			<div id="meuSlider" class="carousel slide" data-ride="carousel">
+    			<ol class="carousel-indicators">
+        			<li data-target="#meuSlider" data-slide-to="0" class="active"></li>
+        			<li data-target="#meuSlider" data-slide-to="1"></li>
+        			<li data-target="#meuSlider" data-slide-to="2"></li>
+    			</ol>
+    <div class="carousel-inner">
+        	<div class="item active"><img src="mercado_de_servicos.jpeg" alt="Slider 1" /></div>
+        	<div class="item"><img src="servicos.jpeg" alt ="Slide 2" /></div>
+        	<div class="item"><img src="escolha.jpeg" alt="Slide 3" /></div>
+    </div>
+    <a class="left carousel-control" href="#" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a class="right carousel-control" href="#" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+
+
 	</head>
 	<body>
 		 
