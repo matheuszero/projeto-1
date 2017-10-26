b538768404e21e7268639de2825c88f4e592d4ad
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2017-10-25 21:09:04 -0300

    Implementado o login com bd

--- verificacao.php
+++ verificacao.php
@@ -1,18 +1,33 @@
 <?php
-	session_start();
-	include "header.php";
-	$login = $_POST['input_User'];
-	$senha = $_POST['input_Pass'];
-	$_SESSION['logado'] = false;
-
-	if (isset($_SESSION['dados'])) {
-		foreach ($_SESSION['dados'] as $item) {
-			if ($login == $item[0] && $senha == $item[1]) {
-				$_SESSION['logado'] = true;
-				header("location:index.php");
-			}
-		}
-			echo "Senha ou nome estão errados. Ou você não é cadastrado!";
-	} 
-	
+session_start();
+$host = "localhost";
+$banco = "id2900246_financasecustos";
+$user = "id2900246_rhodolfo";
+$pass = "rfm2017";
+$conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error());
+mysqli_select_db($conexao, $banco) or die(mysqli_error());
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Autenticação</title>
+</head>
+<body>
+
+	<?php  
+	$login = $_POST['login'];
+	$senha = $_POST['senha'];
+	$sql = mysqli_query($conexao,"SELECT * FROM LoginUser WHERE Nome_User = '$login' and Senha_User = '$senha'") or die(mysql_error('Login ou senha errado'));
+	$row = mysqli_num_rows($sql);
+	if($row > 0){
+		$_SESSION['login'] = $login;
+		$_SESSION['senha'] = $senha;
+		$_SESSION['logado'] = True;
+		header('loocation: home.php');
+
+	}
+?>
+
+</body>
+</html>
