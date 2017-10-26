4b1c857a631933dcc94464b366695b40d49bd0ec
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2017-10-26 12:24:00 -0300

    Cadastro em BD

--- verificacao.php
+++ verificacao.php
@@ -6,28 +6,17 @@ $user = "id2900246_rhodolfo";
 $pass = "rfm2017";
 $conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error());
 mysqli_select_db($conexao, $banco) or die(mysqli_error());
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Autenticação</title>
-</head>
-<body>
 
-	<?php  
-	$login = $_POST['login'];
-	$senha = $_POST['senha'];
-	$sql = mysqli_query($conexao,"SELECT * FROM LoginUser WHERE Nome_User = '$login' and Senha_User = '$senha'") or die(mysql_error('Login ou senha errado'));
-	$row = mysqli_num_rows($sql);
-	if($row > 0){
-		$_SESSION['login'] = $login;
-		$_SESSION['senha'] = $senha;
-		$_SESSION['logado'] = True;
-		header('loocation: home.php');
+$login = $_POST['input_User'];
+$senha = $_POST['input_Pass'];
+$sql = mysqli_query($conexao,"SELECT * FROM LoginUser WHERE Nome_User = '$login' and Senha_User = '$senha'") or die(mysql_error('Login ou senha errado'));
+$row = mysqli_num_rows($sql);
+if($row > 0){
+	$_SESSION['login'] = $login;
+	$_SESSION['logado'] = True;
+	echo "<script>location.href='home.php';</script>"; 
+	
+}
 
-	}
 ?>
 
-</body>
-</html>
